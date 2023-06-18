73831d9ce48303a49e0cb70b2b353ce27948e42b
Mica Zayas <micaelazayas89@gmail.com>
2023-06-18 02:38:12 +0200

    se agrega carrousel de vuetify

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,41 @@
 <template>
-  <hello-world />
-
-  
-
-  
+  <div>
+    <v-carousel :show-arrows="false">
+      <v-carousel-item
+        v-for="(item, i) in items"
+        :key="i"
+        :src="item.src"
+        class="p-0 m-0">
+      </v-carousel-item>
+    </v-carousel>
+  </div>
 </template>
 
-<script>
-  import HelloWorld from '../components/HelloWorld'
+ 
+//<script>
+//import Favoritos from "../components/Favoritos.vue";
 
-  export default {
-    name: 'HomeView',
+export default {
+  name: "HomeView",
+  //components: { Favoritos },
 
-    components: {
-      HelloWorld,
+  data: () => ({
+    items: [
+    {
+      src: require("@/assets/frutas.jpg"),
     },
-  }
+    {
+      src: require("@/assets/chocolate.jpg"),
+    },
+    {
+      src: require("@/assets/crema.webp"),
+    },
+    {
+      src: require("@/assets/postre-choco.jpg"),
+    },
+   ],
+
+  }),
+
+};
 </script>
