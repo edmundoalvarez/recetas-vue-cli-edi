6cf6d400e9c91fb6563f5df3b847ccfd54331229
Edmundo Alvarez <edmundoalvarezok@gmail.com>
2023-06-22 08:27:58 -0300

    EditarComponent

--- src/views/EditarView.vue
+++ src/views/EditarView.vue
@@ -1,143 +1,18 @@
 <template>
-    <div class="container">
-      <h1 class="brown--text">Editá la receta</h1>
-        <v-form ref="form">
-  
-          <v-text-field
-            v-model="form_data.recetaNombre"
-            label="Nombre"
-            counter="20"
-            >
-          </v-text-field>
-  
-          <v-textarea
-            label="Receta"
-            v-model="form_data.recetaDescripcion"
-            rows="2"
-            row-height="20">
-          </v-textarea>
-  
-          <v-select
-            :items="items"
-            label="Categoría del postre"
-            multiple
-            v-model="form_data.selected">
-          </v-select>
-  
-          <v-text-field
-            v-model="form_data.origen"
-            label="Origen/Historia">
-          </v-text-field>
-  
-        <div class="guardar">
-          <v-btn rounded color="amber" dark @click="guardar(form_data)">
-            Guardar
-          </v-btn>
-        </div>
-      </v-form>
-  
-      <div>
-        <div v-if="control === true">
-          <div v-if="cantidadErrores" :class="muestraError">
-            <ul>
-              <li v-for="x in errores" :key="x">{{ x }}</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-    </template>
-    
-    <script>
-    export default {
-      name: "EditarView",
+  <v-container>
+      <EditarComponent></EditarComponent>
+  </v-container>
+</template>
 
-      props : ["id"],
-    
-      data: function () {
-        return {
-          form_data: {},
-          recetasCargadas : [],
-          items: ["Chocolate", "Vainilla", "Crema", "Fruta", "Otro"],
-          control: false,
+<script>
+import EditarComponent from "../components/Editar.vue";
 
-        };
-      },
-    
-      computed: {
-        cantidadErrores: function () {
-          return this.errores.length; // Devolvemos la de cantidad errores
-        },
-      },
-    
-      methods: {
-        guardar: function (form_data) {
-    
-          this.control = true; //evaluamos que los mensajes se ejecuten sólo cuando se ejecute la función
-          this.errores = []; //vaciamos el array de errores
-        
-          if (!this.form_data.recetaNombre) {
-            this.errores.push("El nombre es obligatorio.");
-          }
-    
-          if (this.form_data.recetaNombre && this.form_data.recetaNombre.length < 5) {
-            this.errores.push("El nombre de la receta debe tener más de 5 caracteres.");
-          }
-    
-          if (!this.form_data.recetaDescripcion) {
-            this.errores.push("La descriçión de la receta no puede estar vacía.");
-          }
-        
-          if (form_data.selected.length === 0) {
-            this.errores.push("Debe seleccionar una categoría.");
-          }
-    
-          if (!form_data.origen) {
-            this.errores.push("El origen o historia de tu receta no puede estar vacía.");
-          }
-    
-          if (this.errores.length == 0) {
-            form_data = Object.assign({}, form_data, { id: new Date().getTime() });
-    
-            if (!localStorage.form) {
-              this.datosForm = [];
-            } else {
-              this.datosForm = JSON.parse(localStorage.getItem("form"));
-            }
-            
-            this.recetasCargadas.indexOf(form_data.id);
-            localStorage.setItem("form", JSON.stringify(this.recetasCargadas));
-            this.$router.push("/recetasguardadas");
-            console.log(this.recetasCargadas)
-          }
-        },
 
-        recetaSeleccionada: function (){
-            if(this.recetasCargadas.length > 0) {
+export default {
+name: "EditarView",
+components: { 
+  EditarComponent
+},
 
-                for (const d of this.recetasCargadas) {
-
-                    if(this.$route.params.id == d.id){
-                        this.form_data = d;
-                    }
-                    
-                }
-
-            }
-        }
-      },
-      mounted: function(){
-
-        if (!localStorage.form) {
-        this.recetasCargadas = [];
-        
-        } else {
-        this.recetasCargadas = JSON.parse(localStorage.getItem("form"));
-        
-        }
-
-        this.recetaSeleccionada();
-
-      },
-    };
-    </script>
+};
+</script>
